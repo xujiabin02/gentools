351c091791b5e3f52ce62d753760a3c0902d0be5
yanggang <yanggang@9fbank.cc>
2019-08-09 12:19:01 +0800

    增加文件写入函数(覆盖写入)

--- fileOper.go
+++ fileOper.go
@@ -7,15 +7,31 @@ import (
 )
 
 func ReadFile(fileName string) string {
-	var resultRturn string
+	var resultReturn string
 	//fileName := "./logconfig.json"
 	if fileObj, err := os.Open(fileName); err == nil {
 		defer fileObj.Close()
 		if contents, err := ioutil.ReadAll(fileObj); err == nil {
-			resultRturn = strings.Replace(string(contents), "\n", "", 1)
+			resultReturn = strings.Replace(string(contents), "\n", "", 1)
 			//fmt.Println("Use os.Open family functions and ioutil.ReadAll to read a file contents:",result)
 		}
 
 	}
-	return resultRturn
+	return resultReturn
+}
+func WriteFile(fileName string, content string) string {
+	var d1 = []byte(content)
+	err2 := ioutil.WriteFile(fileName, d1, 0666) //写入文件(字节数组)
+	if err2 != nil {
+		panic(err2)
+	}
+	return "OK"
+}
+
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
 }
